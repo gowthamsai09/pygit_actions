737ba8ec2d2e87d49ee8c4196d276517d9391c75
gowthamsai09 <gowthamsaiyadlapalli1234@gmail.com>
2026-02-03 21:18:02 +0530

    first commit

---
+++ tests/test_ops.py
@@ -0,0 +1,11 @@
+from src.ops import add, sub
+
+def test_add():
+    assert add(10, 2) == 12
+    assert add(2, 3) == 5
+    assert add(-1, 10) == 9
+
+def test_sub():
+    assert sub(4, 3) == 1
+    assert sub(2, 3) == -1
+    assert sub(12, 10) == 2
